4706db2de29db7ae743aa5de57a3991879ce2e15
depinindocyber <depin.sutohap@indocyber.id>
2025-07-03 21:23:37 +0700

    init ws

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer();
+const io = new Server(server, {
+  cors: {
+    origin: '*', // Adjust as needed for security
+  }
+});
+
+let data = [];
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  socket.join(socket.room);
+  if (!data[socket.room]) {
+    data[socket.room] = {};
+  }
+  data[socket.room][socket.id] = { x: socket.x, y: socket.y, user: socket.id };
+  io.to(socket.room).emit("position", data[socket.room]);
+  socket.on('disconnect', () => {
+    delete data[socket.room][socket.id];
+    io.to(socket.room).emit("position", data[socket.room][socket.id]);
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`);
+});
